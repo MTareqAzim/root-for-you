0d9a8f80a54a6be76330ab738b0f7beb2603ad92
Tareq Anuar <MTareqAzim@gmail.com>
2023-02-04 06:43:21 +0800

    Got scene changing working.

--- scripts/godot_splash.gd
+++ scripts/godot_splash.gd
@@ -1,8 +1,32 @@
 extends Control
 
+export(float) var fade_in_delay := 1.0
+export(float) var fade_in_time := 2.0
+export(float) var fade_out_delay := 3.0
+export(float) var fade_out_time := 1.5
 export(String, FILE, "*.tscn") var scene_to_preload
+
+onready var logo = $GodotLogo
+onready var tween = $Tween
+
+
+func _ready():
+	logo.modulate = Color.transparent
 
 
 func poll_preload_scene() -> void:
-	if scene_to_preload:
-		get_viewport().preload_scene(scene_to_preload)
+	get_viewport().preload_scene(scene_to_preload)
+	_set_up_and_start_tween()
+
+
+func _set_up_and_start_tween() -> void:
+	var fade_out_final_delay := fade_in_delay + fade_in_time + fade_out_delay
+	var change_scene_delay := fade_out_final_delay + fade_out_time
+	
+	tween.interpolate_property(logo, "modulate", Color.transparent, Color.white, fade_in_time,
+			Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, fade_in_delay)
+	tween.interpolate_property(logo, "modulate", Color.white, Color.transparent, fade_out_time,
+			Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, fade_out_final_delay)
+	tween.interpolate_deferred_callback(get_viewport(), change_scene_delay, "change_to_preloaded_scene")
+	
+	tween.start()
--- scripts/scene_manager.gd
+++ scripts/scene_manager.gd
@@ -18,7 +18,8 @@ func change_scene(path: String) -> void:
 	get_child(0).queue_free()
 	add_child(new_scene)
 	
-	_clear_preloaded_scene()
+	preloaded_scene.queue_free()
+	preloaded_scene = null
 
 
 func change_to_preloaded_scene() -> void:
@@ -28,19 +29,13 @@ func change_to_preloaded_scene() -> void:
 	get_child(0).queue_free()
 	add_child(preloaded_scene)
 	
-	_clear_preloaded_scene()
+	preloaded_scene = null
 
 
 func preload_scene(path: String) -> void:
-	_clear_preloaded_scene()
+	if preloaded_scene:
+		if not preloaded_scene.is_inside_tree():
+			preloaded_scene.queue_free()
 	
 	var new_scene_resource = load(path)
 	preloaded_scene = new_scene_resource.instance()
-
-
-func _clear_preloaded_scene() -> void:
-	if not preloaded_scene:
-		return
-	
-	preloaded_scene.free()
-	preloaded_scene = null

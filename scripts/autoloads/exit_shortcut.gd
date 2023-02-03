82e5d3a8290df686d63a8947bb6a37b3c776e527
Tareq Anuar <MTareqAzim@gmail.com>
2023-02-04 02:43:26 +0800

    Added exit shortcut autoload.

---
+++ scripts/autoloads/exit_shortcut.gd
@@ -0,0 +1,21 @@
+extends Node
+
+
+func _ready():
+	if OS.get_name() != "FRT":
+		queue_free()
+
+
+func _input(event):
+	if event.is_action_pressed("start"):
+		if Input.is_action_pressed("back"):
+			_exit()
+	
+	if event.is_action_pressed("back"):
+		if Input.is_action_pressed("start"):
+			_exit()
+
+
+func _exit():
+	get_tree().set_input_as_handled()
+	get_tree().notification(MainLoop.NOTIFICATION_WM_QUIT_REQUEST)

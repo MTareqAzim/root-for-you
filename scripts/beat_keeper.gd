16ef3a3eedd49a039a40c5ccdc9191e24d5256e8
Tareq Anuar <MTareqAzim@gmail.com>
2023-02-04 12:59:06 +0800

    Added sounds to beat keeper.

--- scripts/beat_keeper.gd
+++ scripts/beat_keeper.gd
@@ -1,20 +1,24 @@
 extends Control
 
-export(float) var beats_per_minute := 1.0
+export(bool) var enabled := true setget set_enabled
+export(int) var beats_per_minute := 90
 export(bool) var pronounced_beats := false
 export(int, 1, 100) var time_signature := 4
 
-onready var time_between_beats : float = 1.0 / beats_per_minute
+onready var time_between_beats : float = 60.0 / beats_per_minute
 
 onready var pronounced_sound : AudioStreamPlayer = $PronouncedSound
 onready var regular_sound : AudioStreamPlayer = $RegularSound
 onready var beat_display : AnimatedSprite = $Control/BeatDisplay
 
-var nth_beat : int = 0
+var nth_beat : int = -1
 var beat_delta : float = 0.0
 
 
 func _process(delta: float) -> void:
+	if not enabled:
+		return
+	
 	beat_delta += delta
 	
 	if beat_delta < time_between_beats:
@@ -35,3 +39,10 @@ func _beat(count: int) -> void:
 		regular_sound.play()
 		beat_display.play("regular")
 
+
+func set_enabled(value: bool) -> void:
+	if not value and enabled:
+		nth_beat = -1
+		beat_delta = 0.0
+	
+	enabled = value

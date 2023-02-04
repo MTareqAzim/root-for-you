103dc5f92e06cf225c60b54bf11e5a085cf271ee
Tareq Anuar <MTareqAzim@gmail.com>
2023-02-04 19:04:31 +0800

    Update beat keeper's API

--- scripts/beat_keeper.gd
+++ scripts/beat_keeper.gd
@@ -1,9 +1,12 @@
 extends Control
 
+signal beat
+
 export(bool) var enabled := true setget set_enabled
 export(int) var beats_per_minute := 90 setget set_beats_per_minute
 export(bool) var pronounced_beats := false
 export(int, 1, 100) var time_signature := 4
+export(float, 0.0, 0.5) var leeway_ratio := 0.25
 
 onready var time_between_beats : float = 60.0 / beats_per_minute
 
@@ -38,6 +41,8 @@ func _beat(count: int) -> void:
 	else:
 		regular_sound.play()
 		beat_display.play("regular")
+	
+	emit_signal("beat")
 
 
 func set_enabled(value: bool) -> void:
@@ -51,3 +56,23 @@ func set_enabled(value: bool) -> void:
 func set_beats_per_minute(bpm: int) -> void:
 	beats_per_minute = bpm
 	time_between_beats = 60.0 / beats_per_minute
+
+
+func is_closest_beat_pronounced() -> bool:
+	var offset : int = 1 if beat_delta > (time_between_beats*0.5) else 0
+	var closest_beat : int = (nth_beat + offset) % time_signature
+	return closest_beat == 0
+
+
+func is_on_beat() -> bool:
+	return is_within_previous_beat() or is_within_next_beat()
+
+
+func is_within_next_beat() -> bool:
+	var leeway : float = time_between_beats * leeway_ratio
+	return (time_between_beats - beat_delta) <= leeway
+
+
+func is_within_previous_beat() -> bool:
+	var leeway : float = time_between_beats * leeway_ratio
+	return beat_delta <= leeway
